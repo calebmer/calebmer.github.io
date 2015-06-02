baac6f5663a250b54328c37e209b3d5863066e10
Caleb Meredith <calebmeredith8@gmail.com>
2015-06-02 19:06:11 -0400

    Animate transitions

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,16 @@ function rescueOrphans() {
 
 $('p').each(hangPunctuation);
 $('h1,h2,h3,p').each(rescueOrphans);
+
+var durationMs = parseFloat($('.scene-element').css('animation-duration')) * 1000;
+
+var content = $('#container').smoothState({
+  prefetch: true,
+  onStart: {
+    duration: durationMs,
+    render: function (url) {
+      content.toggleAnimationClass('is-exiting');
+      $('html, body').animate({ scrollTop: 0 });
+    }
+  }
+}).data('smoothState');
